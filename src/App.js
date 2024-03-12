475f0e25e658f2cf84dc8db400b5065645da3308
rivlal <gaynikot@gmail.com>
2024-03-12 17:21:42 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <div>
+      <img src="./papaniko.jpg" alt="papaniko" />
+        <h1>Salama aby ô. Dev Papaniko ty!!!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
